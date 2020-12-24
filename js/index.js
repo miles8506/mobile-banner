5b03f80b6f14c97d4777b427236858dff94b7198
miles8506 <74091099+miles8506@users.noreply.github.com>
2020-12-24 09:39:54 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,6 +11,7 @@ window.addEventListener('load', function () {
     var adver = document.querySelector('.advertising');
     var w = adver.offsetWidth;
     var index = 0;
+    var prevent = true;
     var timer = setInterval(function () {
         index++;
         translatex = -index * w;
@@ -18,63 +19,62 @@ window.addEventListener('load', function () {
         adverbar.style.transform = 'translateX(' + translatex + 'px)';
     }, 2000);
     adverbar.addEventListener('transitionend', function () {
-        if (index >= adverbar.children.length - 2) {
-            index = 0;
-            adverbar.style.transition = 'none';
-            translatex = -index * w;
-            adverbar.style.transform = 'translateX(' + translatex + 'px)';
-        } else if (index < 0) {
-            index = adverbar.children.length - 3;
-            adverbar.style.transition = 'none';
-            translatex = -index * w;
-            adverbar.style.transform = 'translateX(' + translatex + 'px)';
+        if (prevent) {
+            prevent = false;
+            if (index >= adverbar.children.length - 2) {
+                index = 0;
+                adverbar.style.transition = 'none';
+                translatex = -index * w;
+                adverbar.style.transform = 'translateX(' + translatex + 'px)';
+            } else if (index < 0) {
+                index = adverbar.children.length - 3;
+                adverbar.style.transition = 'none';
+                translatex = -index * w;
+                adverbar.style.transform = 'translateX(' + translatex + 'px)';
+            }
+            dotbar.querySelector('.current').classList.remove('current');
+            dotbar.children[index].classList.add('current');
+            prevent = true;
         }
-        dotbar.querySelector('.current').classList.remove('current');
-        dotbar.children[index].classList.add('current');
     })
     // 滾動屏幕
     var startX = 0;
     var move = 0;
     var flag = false;
-    var prevent = true;
-    if (prevent) {
-        prevent = false;
-        adverbar.addEventListener('touchstart', function (e) {
-            startX = e.targetTouches[0].pageX;
-            clearInterval(timer);
-        });
-        adverbar.addEventListener('touchmove', function (e) {
-            flag = true;
-            move = e.targetTouches[0].pageX - startX;
-            translatex = -index * w + move;
-            adverbar.style.transition = 'none';
-            adverbar.style.transform = 'translateX(' + translatex + 'px)';
-            e.preventDefault();
-        });
-        adverbar.addEventListener('touchend', function () {
-            if (flag) {
-                if (Math.abs(move) > 50) {
-                    if (move < 0) {
-                        index++;
-                    } else {
-                        index--;
-                    }
-                    moveGet();
+    adverbar.addEventListener('touchstart', function (e) {
+        startX = e.targetTouches[0].pageX;
+        clearInterval(timer);
+    });
+    adverbar.addEventListener('touchmove', function (e) {
+        flag = true;
+        move = e.targetTouches[0].pageX - startX;
+        translatex = -index * w + move;
+        adverbar.style.transition = 'none';
+        adverbar.style.transform = 'translateX(' + translatex + 'px)';
+        e.preventDefault();
+    });
+    adverbar.addEventListener('touchend', function () {
+        if (flag) {
+            if (Math.abs(move) > 50) {
+                if (move < 0) {
+                    index++;
                 } else {
-                    moveGet();
+                    index--;
                 }
-                flag = false;
-                prevent = true;
+                moveGet();
+            } else {
+                moveGet();
             }
-            clearInterval(timer);
-            timer = setInterval(function () {
-                index++;
-                translatex = -index * w;
-                adverbar.style.transition = 'all .3s';
-                adverbar.style.transform = 'translateX(' + translatex + 'px)';
-            }, 2000);
-        })
-    }
+            flag = false;
+        }
+        clearInterval(timer);
+        timer = setInterval(function () {
+            index++;
+            translatex = -index * w;
+            adverbar.style.transition = 'all .3s';
+            adverbar.style.transform = 'translateX(' + translatex + 'px)';
+        }, 2000);
+    })
     function moveGet() {
         translatex = -index * w;
         adverbar.style.transition = 'all .3s';
